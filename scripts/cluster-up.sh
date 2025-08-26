a490d19aa858db8a0636a6743e82fbe2ddb2ea78
Arminnn <arminnn@Mac.fritz.box>
2025-08-26 18:13:28 +0200

    add cluster-up.sh

--- scripts/cluster-up.sh
+++ scripts/cluster-up.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+#start-local.sh to bring up kind + local registry + nginx ingress
 ./start-local.sh
 
-# wait so Ingress is usable immediately for http://localhost
+#wait so Ingress is usable immediately for http://localhost
 kubectl wait -n ingress-nginx \
   --for=condition=Available deploy/ingress-nginx-controller --timeout=180s || true
-
-# kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+##
 echo "[ok] cluster ready"
